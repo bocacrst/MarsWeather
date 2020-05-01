0a4f00a7726040347e598c9f966d9303c740ea79
bocacrst <61462231+bocacrst@users.noreply.github.com>
2020-05-01 14:37:21 +0300

    hgjg

--- script/index.js
+++ script/index.js
@@ -1,4 +1,5 @@
-const API_KEY = 'DEMO_KEY';
+const API_KEY = 'pDczkRYHON09nXHUmsCsPCqZ7Epjyt8yEChvfhnK';
+//const API_KEY = 'DEMO_KEY';
 const API_URL = `https://api.nasa.gov/insight_weather/?api_key=${API_KEY}&feedtype=json&ver=1.0`;
 
 const previousWeatherButton = document.querySelector('.show-previous-weather-button');
@@ -11,16 +12,47 @@ const windSpeedElement = document.querySelector('[data-wind-speed]');
 const windDirectionArrow = document.querySelector('.wind__arrow');
 const windDirectionText = document.querySelector('[data-wind-direction-text]');
 
+const previousSolContainer = document.querySelector('.previous-days');
+const previousSolTemplate = document.querySelector('[data-previous-sol-template]');
+
+const unitToggle = document.querySelector('.unit__toggle');
+const metricRadio = document.querySelector('#cel');
+const imperialRadio = document.querySelector('#fah');
+
 previousWeatherButton.addEventListener('click', e =>{
     previousWeather.classList.toggle('show-weather');
 })
 
 let selectedSolIndex;
+function isMetric(){
+    return metricRadio.checked;
+}
 
 getWeather().then(sols => {
     selectedSolIndex = sols.length -1;
     displaySelectedSol(sols);
-    console.log(sols);
+    displayPreviousSols(sols);
+    updateUnits();
+
+    unitToggle.addEventListener('click', e =>{
+        let notMetric = !isMetric();
+        metricRadio.checked = notMetric;
+        imperialRadio.checked = !notMetric;
+        updateUnits();
+        displaySelectedSol(sols);
+       
+    })
+
+    metricRadio.addEventListener('change', ()=>{
+        updateUnits();
+        displaySelectedSol(sols);
+    })
+
+    imperialRadio.addEventListener('change', ()=>{
+        updateUnits();
+        displaySelectedSol(sols);
+    })
+    
 
 });
 
@@ -32,7 +64,24 @@ function displaySelectedSol(sols) {
     currentTempLowElement.innerText = displayTemperature(selectedSol.minTemp);
     windSpeedElement.innerText = displaySpeed(selectedSol.windSpeed);
     windDirectionArrow.style.setProperty('--direction', `${selectedSol.windDirectionDegrees}deg`);
-    windDirectionText.innerText = selectedSol.windDirectionCardinal;
+   // windDirectionText.innerText = selectedSol.windDirectionCardinal;
+}
+
+function displayPreviousSols(sols){
+    previousSolContainer.innerHTML = ' ';
+    sols.forEach((solData,index) => {
+        const solContainer = previousSolTemplate.content.cloneNode(true);
+        solContainer.querySelector('[data-sol]').innerText = solData.sol;
+        solContainer.querySelector('[data-date]').innerText = displayDate(solData.date);
+        solContainer.querySelector('[data-temp-high]').innerText = displayTemperature(solData.maxTemp);
+        solContainer.querySelector('[data-temp-low]').innerText = displayTemperature(solData.minTemp);
+        
+        solContainer.querySelector('[data-select-button]').addEventListener('click', () =>{
+            selectedSolIndex = index;
+            displaySelectedSol(sols);
+        })
+        previousSolContainer.appendChild(solContainer);
+    });
 }
 
 function displayDate(date){
@@ -40,10 +89,19 @@ function displayDate(date){
 }
 
 function displayTemperature(temp){
+    
+    if(!metricRadio.checked)
+        return Math.round(temp * 1.8 +32);
+    
+   
     return Math.round(temp);
 }
 
 function displaySpeed(speed){
+    if(!metricRadio.checked)
+        return Math.round(speed * (1/1.609));
+
+
     return Math.round(speed);
 }
 
@@ -61,11 +119,25 @@ function getWeather(){
                 minTemp: data.AT.mn,
                 maxTemp: data.AT.mx,
                 windSpeed: data.HWS.av,
-                windDirectionDegrees: data.WD.most_common?.compass_degrees,
+                windDirectionDegrees: data.WD.most_common.compass_degrees,
                 windDirectionCardinal: data.WD.most_common.compass_point,
                 date: new Date(data.First_UTC)
             }
         })
        
     })
+}
+
+function updateUnits(){
+    const tempUnits = document.querySelectorAll('[data-temp-unit]');
+    const speedUnits = document.querySelectorAll('[data-speed-unit]');
+
+    tempUnits.forEach(curr =>{
+        curr.innerText = metricRadio.checked ? 'C':'F';
+    });
+
+    speedUnits.forEach(curr =>{
+        curr.innerText = metricRadio.checked ? 'Km/h':'Mph';
+    });
+
 }
